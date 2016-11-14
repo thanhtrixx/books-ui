d37919cc8fa3aae35afb78d3b7fc2ec7a52cf344
TriLe <thanhtrixx@gmail.com>
2016-11-14 21:21:29 +0700

    update header

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@ const _searchNavBar = () => {
 }
 
 const _dropDown = () => {
-	$('.dropdown-button').dropdown({
+	$('.top-dropdown-button').dropdown({
 		inDuration: 300,
 		outDuration: 225,
 		constrain_width: false,
@@ -26,6 +26,15 @@ const _dropDown = () => {
 		belowOrigin: true,
 		alignment: 'left'
 	})
+	$('.profile-dropdown-button').dropdown({
+		inDuration: 300,
+		outDuration: 225,
+		constrain_width: false,
+		hover: true,
+		gutter: 0,
+		belowOrigin: true,
+		alignment: 'right'
+	})
 }
 
 const _modal = () => {
@@ -33,7 +42,6 @@ const _modal = () => {
 }
 
 const hideSearchNav = () => {
-	console.log('hide')
 	showHideSearchNav()
 	$('#search').val('')
 }
